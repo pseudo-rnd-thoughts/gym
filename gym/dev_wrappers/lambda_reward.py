d290907afebc0b9ed385379e39dddb3b49b4f960
Gianluca De Cola <gianluca.deco@gmail.com>
2022-06-04 15:54:36 +0200

    Update lambda reward. Add lambda reward tests.

--- gym/dev_wrappers/lambda_reward.py
+++ gym/dev_wrappers/lambda_reward.py
@@ -1,3 +1,5 @@
+"""Lambda reward wrappers that uses jumpy for compatibility with jax (i.e. brax) and numpy environments."""
+
 from typing import Callable, Optional, Union
 
 import jumpy as jp
@@ -7,22 +9,30 @@ import gym
 
 
 class lambda_reward_v0(gym.RewardWrapper):
-    """A reward wrapper that allows a custom function to modify the step reward
+    """A reward wrapper that allows a custom function to modify the step reward.
 
     Example:
         >>> import gym
         >>> env = gym.make("CartPole-v1")
         >>> env = lambda_reward_v0(env, lambda r: 2 * r + 1)
         >>> env.reset()
-        >>> env.step(0)
-        TODO
+        >>> _, rew, _, _ = env.step(0)
+        >>> rew
+        3.0
     """
-    def __init__(self, env: gym.Env, fn: Callable[[Union[float, int, np.ndarray]], Union[float, int, np.ndarray]]):
+
+    def __init__(
+        self,
+        env: gym.Env,
+        fn: Callable[[Union[float, int, np.ndarray]], Union[float, int, np.ndarray]],
+    ):
+        """Initialize lambda_reward_v0 wrapper."""
         super().__init__(env)
 
         self.fn = fn
 
     def reward(self, reward):
+        """Apply function to reward."""
         return self.fn(reward)
 
 
@@ -32,20 +42,25 @@ class clip_rewards_v0(lambda_reward_v0):
     Example with an upper and lower bound:
         >>> import gym
         >>> env = gym.make("CartPole-v1")
-        >>> env = clip_rewards_v0(env, -1, 5)
+        >>> env = clip_rewards_v0(env, 0, 0.5)
         >>> env.reset()
-        >>> env.step(1)
-        TODO
+        >>> _, rew, _, _ = env.step(1)
+        >>> rew
+        0.5
     """
+
     def __init__(
         self,
         env: gym.Env,
-        min_reward: Optional[float | jp.ndarray] = None,
-        max_reward: Optional[float | jp.ndarray] = None,
+        min_reward: Optional[Union[float, jp.ndarray]] = None,
+        max_reward: Optional[Union[float, jp.ndarray]] = None,
     ):
+        """Initialize clip_reward_v0 wrapper."""
         if min_reward is None and max_reward is None:
-            raise Exception("Both `min_reward` and `max_reward` cannot be None")  # TODO update exception
-        elif max_reward < min_reward:
+            raise Exception(
+                "Both `min_reward` and `max_reward` cannot be None"
+            )  # TODO update exception
+        elif max_reward and min_reward and max_reward < min_reward:
             raise Exception(
                 f"Min reward ({min_reward}) must be less than max reward ({max_reward})"  # TODO update exception
             )
---
+++ tests/dev_wrappers/test_lambda_rewards.py
@@ -0,0 +1,78 @@
+"""Test lambda reward wrapper."""
+import pytest
+
+import numpy as np
+
+import gym
+from gym.wrappers import clip_rewards_v0, lambda_reward_v0
+
+ENV_ID = "CartPole-v1"
+NUM_ENVS = 3
+SEED = 1
+
+
+@pytest.mark.parametrize(
+    (
+        "reward_fn",
+        "expected_reward",
+    ),
+    [(lambda r: 2 * r + 1, 3)],
+)
+def test_lambda_reward(reward_fn, expected_reward):
+    env = gym.make(ENV_ID)
+    env = lambda_reward_v0(env, reward_fn)
+    env.reset(seed=SEED)
+    _, rew, _, _ = env.step(0)
+    assert rew == expected_reward
+
+
+@pytest.mark.parametrize(
+    ("lower_bound", "upper_bound", "expected_reward"),
+    [(None, 0.5, 0.5), (0, None, 1), (0, 0.5, 0.5)],
+)
+def test_clip_reward(lower_bound, upper_bound, expected_reward):
+    env = gym.make(ENV_ID)
+    env = clip_rewards_v0(env, lower_bound, upper_bound)
+    env.reset(seed=SEED)
+    _, rew, _, _ = env.step(0)
+
+    assert rew == expected_reward
+
+
+@pytest.mark.parametrize(("lower_bound", "upper_bound"), [(None, None), (1, -1)])
+def test_clip_reward_incorrect_params(lower_bound, upper_bound):
+    env = gym.make(ENV_ID)
+    with pytest.raises(Exception):
+        env = clip_rewards_v0(env, lower_bound, upper_bound)
+
+
+@pytest.mark.parametrize(
+    (
+        "reward_fn",
+        "expected_reward",
+    ),
+    [(lambda r: 2 * r + 1, 3)],
+)
+def test_lambda_reward_within_vector(reward_fn, expected_reward):
+    ACTIONS = [0 for _ in range(NUM_ENVS)]
+
+    env = gym.vector.make(ENV_ID, num_envs=NUM_ENVS)
+    env = lambda_reward_v0(env, reward_fn)
+    env.reset(seed=SEED)
+    _, rew, _, _ = env.step(ACTIONS)
+    assert np.alltrue(rew == expected_reward)
+
+
+@pytest.mark.parametrize(
+    ("lower_bound", "upper_bound", "expected_reward"),
+    [(None, 0.5, 0.5), (0, None, 1), (0, 0.5, 0.5)],
+)
+def test_clip_reward_within_vector(lower_bound, upper_bound, expected_reward):
+    ACTIONS = [0 for _ in range(NUM_ENVS)]
+
+    env = gym.vector.make(ENV_ID, num_envs=NUM_ENVS)
+    env = clip_rewards_v0(env, lower_bound, upper_bound)
+    env.reset(seed=SEED)
+    _, rew, _, _ = env.step(ACTIONS)
+
+    assert np.alltrue(rew == expected_reward)
